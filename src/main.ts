83610caa925b222ea47bd2ee8a7f672e7e913536
Lior <lior@l01015153.lan>
2020-12-29 16:44:08 +0100

    v1

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function run() {
     const state = core.getInput("state") as DeploymentState;
 
     const client = new github.GitHub(token, { previews: ["flash", "ant-man"] });
-
+    console.log(context)
     await client.repos.createDeploymentStatus({
       ...context.repo,
       deployment_id: parseInt(deploymentId),
