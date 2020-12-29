19bbc04860792306b1326899d9d3c17e9a4ddea2
Lior <lior@l01015153.lan>
2020-12-29 17:07:19 +0100

    v1

--- src/main.ts
+++ src/main.ts
@@ -15,24 +15,22 @@ async function run() {
   try {
     const context = github.context;
     const defaultUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
-
+console.log(context)
     const payloadStr = JSON.stringify(context.payload, undefined, 2)
     const payload = JSON.parse(payloadStr)
 
     const token = fs.readFileSync('/home/runner/.gittoken','utf8').replace("\n", "");
     const url = core.getInput("target_url", { required: false }) || defaultUrl;
     const description = core.getInput("description", { required: false }) || "";
-    const deploymentId = core.getInput("deployment_id");
     const environmentUrl =
       core.getInput("environment_url", { required: false }) || "";
     const state = core.getInput("state") as DeploymentState;
 
     const client = new github.GitHub(token, { previews: ["flash", "ant-man"] });
-    console.log(payload["deployment"])
-    console.log(payload.deployment.id)
+
     await client.repos.createDeploymentStatus({
       ...context.repo,
-      deployment_id: parseInt(deploymentId),
+      deployment_id: parseInt(payload.deployment.id),
       state,
       log_url: defaultUrl,
       target_url: url,
